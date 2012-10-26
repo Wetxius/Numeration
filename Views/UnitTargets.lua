fdf74890a7272aa403f699cf630d079e65936ec2
Wetxius <wetxius@gmail.com>
2012-10-26 16:28:00 +0400

    Update version to 1.5.

--- Views/UnitTargets.lua
+++ Views/UnitTargets.lua
@@ -19,9 +19,9 @@ function view:Init()
 	local t = addon.types[addon.nav.type]
 	local text
 	if u.owner then
-		text = format("%s"..l.Targets..": %s <%s>", t.name, u.name, u.owner)
+		text = format("%s "..l.Targets..": %s <%s>", t.name, u.name, u.owner)
 	else
-		text = format("%s"..l.Targets..": %s", t.name, u.name)
+		text = format("%s "..l.Targets..": %s", t.name, u.name)
 	end
 	addon.window:SetTitle(text, t.c[1], t.c[2], t.c[3])
 	addon.window:SetBackAction(backAction)
