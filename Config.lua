3531390e209f92a6a4f77fdc4ee610390c70d89a
Wetxius <wetxius@gmail.com>
2013-04-12 18:30:08 +0400

    Added descriptions in Config.

--- Config.lua
+++ Config.lua
@@ -5,46 +5,48 @@ local l = n.locale
 --	Window options
 ----------------------------------------------------------------------------------------
 n["windows"] = {
-	pos = {"TOPLEFT", 4, -4},
-	width = 280,
-	maxlines = 9,
-	backgroundalpha = 0.6,
-	fontshadow = true,
-	scrollbar = true,
+	pos = {"TOPLEFT", 4, -4},				-- Position for Main Frame
+	width = 280,							-- Width for Main Frame
+	maxlines = 9,							-- Maximum lines
+	backgroundalpha = 0.6,					-- Alpha for background
+	fontshadow = true,						-- Use shadow for all fonts
+	scrollbar = true,						-- Show scrollbar
 
-	titleheight = 16,
-	titlealpha = 0.9,
-	titlefont = [[Fonts\ARIALN.TTF]],
-	titlefontstyle = "NONE",
-	titlefontsize = 13,
-	titlefontcolor = {1, 1, 1},
-	buttonhighlightcolor = {1, .82, 0},
+	-- Title
+	titleheight = 16,						-- Heigth for title
+	titlealpha = 0.9,						-- Alpha for title
+	titlefont = [[Fonts\ARIALN.TTF]],		-- Set font for title
+	titlefontstyle = "NONE",				-- Font style for title
+	titlefontsize = 13,						-- Font size for title
+	titlefontcolor = {1, 1, 1},				-- Font color for title
+	buttonhighlightcolor = {1, .82, 0},		-- Color for button highlight
 
-	lineheight = 14,
-	linegap = 1,
-	linealpha = 1,
-	linetexture = [[Interface\Tooltips\UI-Tooltip-Background]],
-	linefont = [[Fonts\ARIALN.TTF]],
-	linefontstyle = "NONE",
-	linefontsize = 11,
-	linefontcolor = {1, 1, 1},
+	-- Lines
+	lineheight = 14,						-- Heigth for lines
+	linegap = 1,							-- Heigth for line gap
+	linealpha = 1,							-- Alpha for lines
+	linefont = [[Fonts\ARIALN.TTF]],		-- Set font for line
+	linefontstyle = "NONE",					-- Font style for line
+	linefontsize = 11,						-- Font size for line
+	linefontcolor = {1, 1, 1},				-- Font color for line
+	linetexture = [[Interface\Tooltips\UI-Tooltip-Background]],	-- Set texture for line
 }
 
 ----------------------------------------------------------------------------------------
 --	Core options
 ----------------------------------------------------------------------------------------
 n["core"] = {
-	refreshinterval = 1,
-	minfightlength = 15,
-	combatseconds = 3,
-	shortnumbers = true,
-	silent_reset = false,
+	refreshinterval = 1,					-- How often to update the numbers
+	minfightlength = 15,					-- Time after which the segment will be saved
+	combatseconds = 3,						-- Time until new segment will be start
+	shortnumbers = true,					-- Use short numbers ("19.2k" instead of "19234")
+	silent_reset = false,					-- Confirm reset data and hide pop-up.
 }
 
 ----------------------------------------------------------------------------------------
 --	Available types and their order
 ----------------------------------------------------------------------------------------
-n.types = {
+n["types"] = {
 	{
 		name = DAMAGE,
 		id = "dd",
